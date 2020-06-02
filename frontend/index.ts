e9d0e75edaffcbe88ef565d682d1f81ba8d817ae
Viktor Lukashov <viktor@vaadin.com>
2020-06-02 17:16:36 +0300

    keep the splash screen until the first server-side view is loaded

--- frontend/index.ts
+++ frontend/index.ts
@@ -13,4 +13,29 @@ const routes = [
 const router = new Router(document.querySelector('#outlet'));
 router.setRoutes(routes);
 
-document.querySelector('#splash-screen')!.classList.add('loaded');
+// If the app has client-side views, the splash screen should be hidden already
+// at this point with
+// document.querySelector('#splash-screen')!.classList.add('loaded');
+//
+// However, since this app has no client-side views, it would be too early to
+// hide the splash screen until also loading a server-side view. Unfortunately,
+// there is no public API to do that in Vaadin 16.0.0
+// (see https://github.com/vaadin/flow/issues/8486)
+
+// WARNING: accessing internal Flow APIs
+// This may break on any Flow version upgrade without any prior notice
+const original = (window as any).Vaadin.Flow.loading;
+(window as any).Vaadin.Flow.loading = (action: boolean) => {
+  // When this is called first time (action = true), do nothing.
+  // That's when Flow starts loading and the splash screen is still visible.
+  // There is no need to show the Flow loading indicator at that time.
+
+  // When this is called second time (action = false), hide the splash screen
+  // and restore the default behavior.
+  // Next time Flow starts loading something the default loading indicator will
+  // be visible.
+  if (!action) {
+    document.querySelector('#splash-screen')!.classList.add('loaded');
+    (window as any).Vaadin.Flow.loading = original;
+  }
+};
